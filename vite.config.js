b6f525987d0eb8f7ca5298235ac482f3281db9f0
zloy <ilya.sidorovich006@gmail.com>
2025-09-11 00:45:25 +0300

    create vite

--- vite.config.js
+++ vite.config.js
@@ -18,7 +18,7 @@ export default defineConfig({
                 main: './src/index.html',
                 pricing: './src/pricing.html',
                 contact: './src/contact.html',
-                about: './src/team.html',
+                about: './src/about.html',
                 blog: './src/blog.html',
                 detailBlog: './src/detailBlog.html',
             },
